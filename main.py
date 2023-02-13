ed5c8ea000e987b1aa96f0b3216dbf0348092904
s9iper1 <s9iper1@gmail.com>
2023-02-13 16:39:14 +0500

    add dynamic support for amd's folder

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import os
+
 import inotify.adapters
 
 
 def main():
     i = inotify.adapters.Inotify()
+    base_target = '/sys/class/backlight'
+    dynamic_amd_folder_current_name = ''
     nvidia_target = '/sys/class/backlight/nvidia_wmi_ec_backlight/brightness'
-    amd_target = '/sys/class/backlight/amdgpu_bl1/brightness'
+    directory_list = list()
+    for root, dirs, files in os.walk(base_target, topdown=False):
+        for name in dirs:
+            if 'amdgpu_bl' in name.strip():
+                dynamic_amd_folder_current_name = name
+                print(name)
+
+    amd_target = f'/sys/class/backlight/{dynamic_amd_folder_current_name}/brightness'
     i.add_watch(nvidia_target)
 
     for event in i.event_gen(yield_nones=False):
